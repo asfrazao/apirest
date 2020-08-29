bb336ebb9471f8469b05edff4b3c67655d147134
Alessandro Frazão <asfrazao@hotmail.com>
2020-08-28 22:57:34 -0300

    Commit Total 28/08/2020

--- src/main/java/com/produtos/apirest/repsitory/ProdutoRepository.java
+++ src/main/java/com/produtos/apirest/repsitory/ProdutoRepository.java
@@ -3,5 +3,8 @@ package com.produtos.apirest.repsitory;
 import com.produtos.apirest.models.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    Produto findById(long id);
 }
--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -1,11 +1,51 @@
 package com.produtos.apirest.resources;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.produtos.apirest.models.Produto;
+import com.produtos.apirest.repsitory.ProdutoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Produtis")
+@CrossOrigin(origins = "*") /* limita o dominio de acesso*/
 public class ProdutoResource {
 
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    @GetMapping("/produtos")
+    @ApiOperation(value="Retorna uma lista de produtos")
+    public List<Produto> listaProdutos(){
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping("/produto/{id}")
+    @ApiOperation(value="Retorna um unico produto")
+    public Produto listaProdutoUnico(@PathVariable(value="id")long id){
+        return produtoRepository.findById(id);
+    }
+
+    @PostMapping("/produto")
+    @ApiOperation(value="Salva um unico produto")
+    public Produto salvaProduto(@RequestBody Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    @DeleteMapping("/produto")
+    @ApiOperation(value="Deleta um produto")
+    public void deletaProduto(@RequestBody Produto produto) {
+        produtoRepository.delete(produto);
+    }
+
+    @PutMapping("/produto")
+    @ApiOperation(value="Atualiza um produto")
+    public Produto atualizaProduto(@RequestBody Produto produto) {
+       return produtoRepository.save(produto);
+    }
 
 }
